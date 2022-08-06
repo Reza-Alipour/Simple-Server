bb0ab1b0190c11ea5ca315ca7b2e3df55c2605eb
Reza Alipour <56475612+Reza-Alipour@users.noreply.github.com>
2022-08-07 00:03:35 +0430

    add some functionality to repository classes

--- src/main/kotlin/com/example/simpleWebServer/repository/CommentRepository.kt
+++ src/main/kotlin/com/example/simpleWebServer/repository/CommentRepository.kt
@@ -1,6 +1,15 @@
 package com.example.simpleWebServer.repository
 
 import com.example.simpleWebServer.entity.Comment
+import com.example.simpleWebServer.entity.Video
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 
-interface CommentRepository : JpaRepository<Comment, Long>
+interface CommentRepository : CrudRepository<Comment, Long> {
+    fun findByVideo(video: Video): List<Comment>
+
+    @Query("select c.text from Comment c where c.video.id = :video_id")
+    fun funcP(@Param("video_id") videoId: Long): List<String>
+}
--- src/main/kotlin/com/example/simpleWebServer/repository/VideoRepository.kt
+++ src/main/kotlin/com/example/simpleWebServer/repository/VideoRepository.kt
@@ -3,7 +3,8 @@ package com.example.simpleWebServer.repository
 import com.example.simpleWebServer.entity.User
 import com.example.simpleWebServer.entity.Video
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.PagingAndSortingRepository
 
-interface VideoRepository : JpaRepository<Video, Long> {
+interface VideoRepository : PagingAndSortingRepository<Video, Long> {
     fun existsVideoByUserAndUserIthVideoAndBanned(user: User, userWithVideo: Int, banned: Boolean): Boolean
 }
